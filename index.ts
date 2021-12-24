cddd0b347f7e161802f211a99d0a91e3cc95b328
Leo <leo.karsbaek@akqa.com>
2021-12-24 15:19:14 +0100

    WIP

--- index.ts
+++ index.ts
@@ -1,14 +1,2 @@
 // Import stylesheets
 import './style.css';
-
-const button = document.querySelector('.enter');
-const overlay = document.querySelector('.overlay');
-
-button.addEventListener('click', () => {
-  overlay.classList.add('hide');
-  overlay.addEventListener('transitionend', (event) => {
-    if (event.target === overlay) {
-      document.body.classList.add('scroll');
-    }
-  });
-});
